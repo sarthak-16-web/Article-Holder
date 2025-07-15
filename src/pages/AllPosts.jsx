6d83c3fa61f12a6ab5bedc868cb231473b7ffe11
Sarthak Kasat <kasat4309@gmail.com>
2025-07-16 02:15:32 +0530

    Update AllPosts.jsx

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -1,28 +1,29 @@
-import React, {useState, useEffect} from 'react'
-import { Container, Postcard } from '../components/import_component'
-import services from "../appwrite/database_services.js";
+
+import { useState, useEffect } from "react"
+import { Container, Postcard } from "../components/import_component"
+import services from "../appwrite/database_services.js"
 
 function AllPosts() {
-    
-    const [posts, setPosts] = useState([])
-    useEffect(() => {
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
     services.GetPosts([]).then((posts) => {
-        if (posts) {
-            setPosts(posts.documents)
-        }
-    });
-    }, []);
+      if (posts) {
+        setPosts(posts.documents)
+      }
+    })
+  }, [])
+
   return (
-    <div className='w-full py-8'>
-        <Container>
-            <div className='flex flex-wrap'>
-                {posts.map((post) => (
-                    <div key={post.$id} className='p-2 w-1/4'>
-                        <Postcard {...post} />
-                    </div>
-                ))}
+    <div className="w-full py-8">
+      <Container>
+        <div className="flex flex-wrap -mx-2">
+          {posts.map((post) => (
+            <div key={post.$id} className="p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+              <Postcard {...post} />
             </div>
-            </Container>
+          ))}
+        </div>
+      </Container>
     </div>
   )
 }
