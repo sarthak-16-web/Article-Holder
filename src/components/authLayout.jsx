d93cd54ee4097f2b924f64b8f4ba5438acb0e096
Sarthak Kasat <kasat4309@gmail.com>
2025-08-12 03:03:33 +0530

    Update authLayout.jsx

--- src/components/authLayout.jsx
+++ src/components/authLayout.jsx
@@ -9,15 +9,6 @@ export default function AuthLayout({children, authentication = true}) {
     const authStatus = useSelector(state => state.auth.status)
 
     useEffect(() => {
-        
-
-        // if (authStatus ===true){
-        //     navigate("/")
-        // } else if (authStatus === false) {
-        //     navigate("/login")
-        // }
-        
-        //let authValue = authStatus === true ? true : false
 
        if(authentication && authStatus !== authentication){
             navigate("/login")
